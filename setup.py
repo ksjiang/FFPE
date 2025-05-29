0ce22fb9868b302bbce97ec87b18419138d7ef60
Kyle <siyu.jiang81@gmail.com>
2025-05-29 10:16:54 -0400

    Fix subpackages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = "FFPE", 
       url = "https://github.com/ksjiang/FFPE", 
       author = "Kyle Jiang", 
       author_email = "siyu.jiang81@gmail.com", 
-      packages = ["FFPE"], 
+      packages = find_packages(), 
       install_requires = ["numpy", "pandas"], 
       version = "0.0.1", 
       license = "MIT", 
